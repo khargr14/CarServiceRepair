60bab9c2887e04cf3a47ef042ba85843c0d24627
keesha <kay_hargrove@yahoo.com>
2023-02-12 21:05:01 -0500

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,8 @@ public class Main
     public static void main( String[] args )
     {
         new CarInfoService().addNewCar(2017,"2OOERTYY","234RTIF");
-new UserinfoService().addNewUser("Aman",2,21);
+new UserinfoService().addNewUser("Kay",2,21);
+        new UserinfoService().addNewUser("Kadin",3,22);
 new UserinfoService().delete(1);
 
     }
